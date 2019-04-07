13291bdf3f850f8b8f19d79b1103af7934a5b7a8
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2019-04-07 15:56:55 +0900

    create

--- main.ts
+++ main.ts
@@ -1,7 +1,24 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+/**
+ * makecode ATP3012 AquesTalk pico Package.
+ */
+
+/**
+ * ATP3012 block
+ */
+//% weight=10 color=#800000 icon="\u1f508" block="atp3012"
+namespace atp3012 {
+    let I2C_ADDR = 0x2e
+    /**
+     * write Data
+     * @param d data to atp3012, eg: "bo'kuwa maikurobittodesu."
+     */
+    //% blockId="wrietData" block="write atp3012 to %dat"
+    //% weight=70 blockGap=8
+    export function write(d: string): void {
+        let buf = pins.createBuffer(d.length);
+        for (let i = 0; i < d.length; i++) {
+            buf[i] = d.charCodeAt(i)
+        }
+        pins.i2cWriteBuffer(I2C_ADDR, buf)
+    }
+}
